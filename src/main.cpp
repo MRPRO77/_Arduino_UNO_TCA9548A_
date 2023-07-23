236a96d612c94a1ba8280cf825e0e7704941cc05
MRPRO77 <mrpro77@hotmail.com>
2023-07-23 20:10:55 -0300

    Login Arduino Uno TCA9548A

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+/*******************************************************************************
+   Título:      TCC Tecnico em Eletrotecnica.
+
+   Descrição:   C L P   ---  I H M
+
+   Processador:  ARDUINO UNO 
+   
+
+
+   Desenvolvido: MRPRO Tecnologia e Automação
+
+   Autor: Tecnologo. Marcelo Rodrigues     Crea-SP 5070505617
+
+   Data de Inicio: 23/07/2023       Versão:   2023.1.1
+
+        
+
+
+*******************************************************************************/  
+/* ============================================================================  
+                                                              
+                                       _                      
+                                      / \                     
+                                     |oo >                    
+                                     _\=/_                    
+                    ___         #   /  _  \   #               
+                   /<> \         \\//|/.\|\\//                
+                 _|_____|_        \/  \_/  \/                 
+                | | === | |          |\ /|                    
+                |_|  0  |_|          \_ _/                    
+                 ||  0  ||           | | |                    
+                 ||__*__||           | | |                    
+                |* \___/ *|          []|[]                    
+                /=\ /=\ /=\          | | |                    
+________________[_]_[_]_[_]_________/_]_[_\_____________________________________
+                                                                             
+************************************************************************************************************/
+
+// --- Bibliotecas Auxiliares ---
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_BusIO_Register.h>
+#include <TCA9548A.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_SSD1306.h>
+
+
+
+
+
+void setup() {
+
+
+
+}
+
+void loop() {
+
+
+
+}
+
+
