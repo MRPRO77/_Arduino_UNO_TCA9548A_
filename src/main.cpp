b16f6f522f3e0fa4a3e4db2e58194b8dc041d64c
MRPRO77 <mrpro77@hotmail.com>
2023-07-28 19:35:14 -0300

    Instalação TCA 9548A Display LCD

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,7 @@ ________________[_]_[_]_[_]_________/_]_[_\_____________________________________
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
+#include "Wire.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_BusIO_Register.h>
 #include <TCA9548A.h>
@@ -47,11 +48,44 @@ ________________[_]_[_]_[_]_________/_]_[_\_____________________________________
 #include <Adafruit_SSD1306.h>
 
 
+// ========================================================================================================
+// --- Definições ---
+
+#define i2clcd       0
+
+#define usalcd    I2CMux.openChannel(i2clcd)
+
+TCA9548A I2CMux;                            // Address can be passed into the constructor
+LiquidCrystal_I2C lcd(0x27,20,4);           // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+
+
+
 
 
 
 void setup() {
 
+                                                    Wire.begin(); 
+                                              Serial.begin(9600);
+                                              I2CMux.begin(Wire);               // Wire instance is passed to the library
+ 
+      
+
+
+                                                           usalcd;
+                                                       lcd.init();
+                                                  lcd.backlight();               
+                                           I2CMux.closeChannel(0);             
+
+
+                                                          usalcd;
+                                                     lcd.clear(); 
+                                              lcd.setCursor(2,1);
+                                   lcd.print("MRPRO TECNOLOGIA");
+                                                      delay(200);
+                                          I2CMux.closeChannel(0);   
 
 
 }
